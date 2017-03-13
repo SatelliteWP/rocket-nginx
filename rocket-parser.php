5511ffbdad0c1c8ae31212c729ba6d45b74bc24e
Maxime Jobin <m@ximejobin.com>
2017-03-12 21:28:02 -0400

    Finished linking the template with the ini file.

--- rocket-parser.php
+++ rocket-parser.php
@@ -14,37 +14,200 @@
 
 class RocketParser {
   
-  public $configFile = "rocket-nginx.ini";
+  public $configFile = 'rocket-nginx.ini';
+  public $templateFile = 'rocket-nginx.tmpl';
   
-  function parseIniFile() {
-    $p_ini = parse_ini_file($this->configFile, true);
+  /**
+   * Parse the ini configuration file
+   */
+  protected function parseIniFile() {
+    $data = parse_ini_file($this->configFile, true);
     $config = array();
-    foreach($p_ini as $namespace => $properties) {
-      list($name, $extends) = explode(':', $namespace);
-      $name = trim($name);
-      $extends = trim($extends);
+
+    foreach($data as $namespace => $properties) {
+      $parts = explode(':', $namespace);
+      $name = trim($parts[0]);
+      $extends = isset($parts[1]) ? trim($parts[1]) : null;
       
       // create namespace if necessary
-      if(!isset($config[$name])) $config[$name] = array();
+      if(!isset($config[$name])) {
+        $config[$name] = array();
+      }
       
       // inherit base namespace
-      if(isset($p_ini[$extends])) {
-        foreach($p_ini[$extends] as $prop => $val)
+      if(isset($data[$extends])) {
+        foreach($data[$extends] as $prop => $val) {
           $config[$name][$prop] = $val;
+        }
       }
       // overwrite / set current namespace values
-      foreach($properties as $prop => $val)
+      foreach($properties as $prop => $val) {
         $config[$name][$prop] = $val;
+      }
     }
+
     return $config;
   }
   
-  public function start() {
+  /**
+   * Generate all configuration files
+   */
+  protected function generateConfigurationFiles($config) {
     
-    if (file_exists($this->configFile) {
+    // Load template
+    $template = $this->getTemplate();
+
+    foreach($config as $name => $section) {
+      $output = $template;
+
+      // Debug
+      $debug = false;
+      if (isset($section['debug']) && $section['debug'] === '1') {
+        $debug = true;
+      }
+      $output = str_replace('#!# DEBUG #!#', $debug ? '1' : '0', $output);
+
+      // WP Content URI
+      $wp_content_folder = 'wp-content';
+      if (isset($section['wp_content_folder']) && !empty($section['wp_content_folder'])) {
+        $wp_content_folder = $section['wp_content_folder'];
+      }
+      $output = str_replace('#!# WP_CONTENT_URI #!#', $wp_content_folder, $output);
+
+      // Cookies
+      $cookies = '';
+      if (isset($section['cookie_invalidate']) && is_array($section['cookie_invalidate'])) {
+        $cookies = implode('|', $section['cookie_invalidate']);
+      }
+      $output = str_replace('#!# COOKIE_INVALIDATE #!#', $cookies, $output);
+
+      // HTTP headers
+      $header_http = '';
+      if (isset($section['http_header']) && is_array($section['http_header'])) {
+        $header_http = $this->getGeneratedHeaders($section['http_header']);
+      }
+      $output = str_replace('#!# HEADER_HTTP #!#', $header_http, $output);
+      
+      // GZIP headers
+      $gzip_header = '';
+      if (isset($section['gzip_header']) && is_array($section['gzip_header'])) {
+        $gzip_header = $this->getGeneratedHeaders($section['gzip_header']);
+      }
+      $output = str_replace('#!# HEADER_GZIP #!#', $gzip_header, $output);
+
+      // Non-GZIP headers
+      $nongzip_header = '';
+      if (isset($section['nongzip_header']) && is_array($section['nongzip_header'])) {
+        $nongzip_header = $this->getGeneratedHeaders($section['nongzip_header']);
+      }
+      $output = str_replace('#!# HEADER_NON_GZIP #!#', $nongzip_header, $output);
+
+      // CSS headers
+      $css_header = '';
+      if (isset($section['css_header']) && is_array($section['css_header'])) {
+        $css_header = $this->getGeneratedHeaders($section['css_header']);
+      }
+      $output = str_replace('#!# HEADER_CSS #!#', $css_header, $output);
+
+      // JS headers
+      $js_header = '';
+      if (isset($section['js_header']) && is_array($section['js_header'])) {
+        $js_header = $this->getGeneratedHeaders($section['js_header']);
+      }
+      $output = str_replace('#!# HEADER_JS #!#', $js_header, $output);
+
+      // Media headers
+      $medias_header = '';
+      if (isset($section['medias_header']) && is_array($section['medias_header'])) {
+        $medias_header = $this->getGeneratedHeaders($section['medias_header']);
+      }
+      $output = str_replace('#!# HEADER_MEDIAS #!#', $medias_header, $output);
+
+      // Media extensions
+      $medias_extension = '';
+      if (isset($section['medias_extension']) && is_array($section['medias_extension'])) {
+        $medias_extension = $this->getGeneratedHeaders($section['medias_extension']);
+      }
+      $output = str_replace('#!# EXTENSION_MEDIAS #!#', $medias_extension, $output);
+      
+
+      // Output the file
+      file_put_contents($name . ".conf", $output);
     }
+
+  }
+
+  /**
+   * Returns generated Nginx headers
+   * @param $headers array Headers
+   *
+   * @return string Nginx headers
+   */
+  protected function getGeneratedHeaders($headers) {
+    $result = '';
+
+    if (isset($headers) && is_array($headers)) {
+      $iteration = 1;
+
+      foreach ($headers as $name => $value) {
+
+        if ($iteration > 1) {
+          $result .= "\t";
+        }
+        $result .= "add_header {$name} \"{$value}\";\n";
+
+        $iteration++;
+      }
+    }
+
+    return $result;
+  }
+
+  /**
+   * Get the template file if it exists
+   */
+  protected function getTemplate() {
+
+    if (file_exists($this->templateFile) === false) {
+      die("Error: the file 'rocket-nginx.ini' could not be found to generate the configuration. " .
+        "You must rename the orginal 'rocket-nginx.ini.disabled' file to 'rocket-nginx.ini' and run this script again.");
+    }
+
+    return file_get_contents('rocket-nginx.tmpl');
+  }
+
+  /**
+   * Check if configuration file exists
+   */
+  protected function checkConfigurationFile() {
+    if (file_exists($this->configFile) === false) {
+      die("Error: the file 'rocket-nginx.ini' could not be found to generate the configuration. " .
+        "You must rename the orginal 'rocket-nginx.ini.disabled' file to 'rocket-nginx.ini' and run this script again.");
+    }    
+  }
+
+  /**
+   * Generate configuration files
+   */
+  public function go() {
+    $this->checkConfigurationFile();
+
+    $data = $this->parseIniFile();
+
+    var_dump($data);
+
+    $this->generateConfigurationFiles($data);
   }
 }
+
+// If file is included, we assume it will call the class automatically.
+// Otherwise, let's generate the configuration files.
+$includedFiles = count(get_included_files());
+if ($includedFiles == 0) {
+  $rp = new RocketParser();
+  $rp->go();  
+}
+
 
 
 
